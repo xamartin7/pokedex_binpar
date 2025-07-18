19716d07d41530f88d746d224c772b55e7284163
xamartin7 <xavimartinalonso@gmail.com>
2025-07-18 16:17:00 +0200

    Providing generations list

---
+++ src/server/modules/generations/application/use-cases/GetGenerationsUseCase.ts
@@ -0,0 +1,22 @@
+import type { IPokeApiRepository } from "@/server/modules/pokemon/infrastructure/interfaces/IPokeApiRepository";
+import type { IGetGenerationsUseCase } from "../interfaces/IGetGenerationsUseCase";
+import type { Generation } from "../../domain/entities/Generation";
+
+export class GetGenerationsUseCase implements IGetGenerationsUseCase {
+    private pokeApiRepository: IPokeApiRepository
+
+    constructor(pokeApiRepository: IPokeApiRepository) {
+        this.pokeApiRepository = pokeApiRepository
+    }
+
+    async getGenerations(): Promise<Generation[]> {
+        const generations = await this.pokeApiRepository.getGenerations()
+        return generations.results.map((generation) => {
+            const id = Number(generation.url.split('/').pop())
+            return {
+                id,
+                name: generation.name
+            }
+        })
+    }
+}
