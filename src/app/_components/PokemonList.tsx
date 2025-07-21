f58444b5eae7279cc88e9213587f299c435d32d5
xamartin7 <xavimartinalonso@gmail.com>
2025-07-21 15:46:26 +0200

    Refactor PaginationSection and PokemonList to improve pagination handling and data flow

--- src/app/_components/PokemonList.tsx
+++ src/app/_components/PokemonList.tsx
@@ -1,40 +1,37 @@
 import type { Generation } from "@/server/modules/generations/domain/entities/Generation";
 import type { Pokemon } from "@/server/modules/pokemon/domain/entities/Pokemon";
-import { useState, useMemo } from "react";
+import { useState } from "react";
 import { PokemonCard } from "./PokemonCard";
 import type { Type } from "@/server/modules/types/domain/entities/Type";
 import { PaginationSection } from "./PaginationSection";
 import { Filters } from "./Filters";
 
-export function PokemonList({initialData}: {initialData: {pokemonList: Pokemon[], generations: Generation[], types: Type[]}}) {
+interface PokemonListProps {
+  initialData: {
+    pokemonList: Pokemon[];
+    generations: Generation[];
+    types: Type[];
+  };
+}
+
+export function PokemonList({initialData}: PokemonListProps) {
   const [pokemonList, setPokemonList] = useState<Pokemon[]>(initialData.pokemonList);
+  const [pokemonListFiltered, setPokemonListFiltered] = useState<Pokemon[]>(initialData.pokemonList);
   const [generations, setGenerations] = useState<Generation[]>(initialData.generations);
   const [types, setTypes] = useState<Type[]>(initialData.types);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [pageSize, setPageSize] = useState(12);
-
-  const paginatedData = useMemo(() => {
-    const startIndex = (currentPage - 1) * pageSize;
-    const endIndex = startIndex + pageSize;
-    return pokemonList.slice(startIndex, endIndex);
-  }, [pokemonList, currentPage, pageSize]);
-
-  const handlePaginationChange = (newCurrentPage: number, newPageSize: number) => {
-    setCurrentPage(newCurrentPage);
-    setPageSize(newPageSize);
-  };
+  const [paginatedData, setPaginatedData] = useState<Pokemon[]>(initialData.pokemonList.slice(0, 12));
 
   return (
     <div className="min-h-screen py-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Filters */}
-        <Filters generations={generations} types={types} pokemonList={pokemonList} setPokemonList={setPokemonList} />
+        <Filters generations={generations} types={types} pokemonList={pokemonList} setPokemonList={setPokemonListFiltered} />
 
         {/* Pagination Section */}
         <div className="mb-6">
           <PaginationSection
-            pokemonList={pokemonList}
-            onPaginationChange={handlePaginationChange}
+            pokemonList={pokemonListFiltered}
+            onPaginatedDataChange={setPaginatedData}
             initialPageSize={12}
           />
         </div>
