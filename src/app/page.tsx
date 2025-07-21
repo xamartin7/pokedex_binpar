b8cd269549883bc3af6392bff527c82830e9bfcf
xamartin7 <xavimartinalonso@gmail.com>
2025-07-21 18:32:33 +0200

    Todos comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,9 @@ import { TitlePage } from "./_components/TitlePage";
 const DEFAULT_GENERATION_ID = 1;
 
 export default function Home() {
+  // TODO Show initial loading at first and then load the pokemon list??
+  // TODO prefetch the pokemon list of the other generations ??
+
   // TODO Use cache
   const { data: generations, isLoading: generationsLoading } = api.pokemon.getGenerations.useQuery();
   const { data: types, isLoading: typesLoading } = api.pokemon.getTypes.useQuery({ generationId: DEFAULT_GENERATION_ID });
