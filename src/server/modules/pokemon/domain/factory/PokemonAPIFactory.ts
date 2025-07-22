3ceb54bcf5ea6867f45cc874e53f72feb423d915
xamartin7 <xavimartinalonso@gmail.com>
2025-07-22 20:07:34 +0200

    Improve error handling

--- src/server/modules/pokemon/domain/factory/PokemonAPIFactory.ts
+++ src/server/modules/pokemon/domain/factory/PokemonAPIFactory.ts
@@ -29,35 +29,42 @@ export class PokemonAPIFactory implements IPokemonFactory {
     }
 
     private async buildPokemon(detailsUrl: string, speciesUrl: string): Promise<Pokemon> {
-        const pokemonDetails = await this.pokeApiRepository.getPokemonDetails(detailsUrl)
-        const pokemonSpecies = await this.pokeApiRepository.getPokemonSpecies(speciesUrl)
-
-        const pokemon: Pokemon = {
-            id: Number(pokemonDetails.id),
-            name: pokemonDetails.name,
-            image: pokemonDetails.sprites.front_default ?? '',
-            types: pokemonDetails.types.map((type) => ({
-                id: Number(type.type.url.split('/')[6]),
-                name: type.type.name,
-                url: type.type.url
-            })),
-            generation: {
-                id: Number(pokemonSpecies.generation.url.split('/')[6]),
-                name: pokemonSpecies.generation.name,
-                url: pokemonSpecies.generation.url
-            },
-            evolutionChain: [],
-            evolutionChainUrl: pokemonSpecies.evolution_chain.url,
-            stats: pokemonDetails.stats.map((stat) => ({
-                base_stat: stat.base_stat,
-                effort: stat.effort,
-                stat: {
-                    name: stat.stat.name,
-                    url: stat.stat.url
-                }
-            }))
-        }
-
-        return pokemon
+        try {
+            const pokemonDetails = await this.pokeApiRepository.getPokemonDetails(detailsUrl)
+            const pokemonSpecies = await this.pokeApiRepository.getPokemonSpecies(speciesUrl)
+            const pokemon: Pokemon = {
+                id: Number(pokemonDetails.id),
+                name: pokemonDetails.name,
+                image: pokemonDetails.sprites.front_default ?? '',
+                types: pokemonDetails.types.map((type) => ({
+                    id: Number(type.type.url.split('/')[6]),
+                    name: type.type.name,
+                    url: type.type.url
+                })),
+                generation: {
+                    id: Number(pokemonSpecies.generation.url.split('/')[6]),
+                    name: pokemonSpecies.generation.name,
+                    url: pokemonSpecies.generation.url
+                },
+                evolutionChain: [],
+                evolutionChainUrl: pokemonSpecies.evolution_chain.url,
+                stats: pokemonDetails.stats.map((stat) => ({
+                    base_stat: stat.base_stat,
+                    effort: stat.effort,
+                    stat: {
+                        name: stat.stat.name,
+                        url: stat.stat.url
+                    }
+                }))
+            }
+    
+            return pokemon
+        } catch (error) {
+            if (error instanceof Error && error.message.includes('404')) {
+                throw new Error('Pokemon not found')
+            }
+            console.error('Error building pokemon:', error)
+            throw error
+        }        
     }
 }
